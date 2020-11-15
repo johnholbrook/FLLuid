efffbec8811218e834a166b01b2ad2064eda2199
John Holbrook <john@johnholbrook.us>
2020-11-15 10:09:32 -0500

    Timer font in the style of new logo

--- display/timer.js
+++ display/timer.js
@@ -40,6 +40,7 @@ var timer_display;
 var play_start_sound = true;
 var play_30sec_sound = true;
 var play_end_sound = true;
+var brick_font = false;
 
 //pre-load sounds
 var start_sound = new Audio('../sounds/charge.mp3');
@@ -49,10 +50,33 @@ var end_sound = new Audio('../sounds/buzzer.mp3');
 //global for sending messages back to controller
 var controllerWindow = remote.getGlobal('controllerWindow');
 
+function textToImage(text){
+    if (text.charAt(1) != ":" || text.length != 4){
+    // if (text.length != 3){
+        console.error("Format: x:yz");
+    }
+    else{
+        let left = text.charAt(0);
+        let mid = text.charAt(2);
+		let right = text.charAt(3);
+        
+		return `<svg class="brick-font" preserveAspectRatio="xMidYMid meet" viewBox="0 0 1600 1200">
+        <image x="0px" y="0px" width="650px" height="1200px" href="../fonts/brick_digits/${left}-left.png"></image>
+        <image x="649px" y="0px" width="470px" height="1200px" href="../fonts/brick_digits/${mid}-mid.png"></image>
+        <image x="1118px" y="0px" width="480px" height="1200px" href="../fonts/brick_digits/${right}-right.png"></image>
+	</svg>`
+    }
+}
+
+function setTimerValue(text){
+	document.querySelector('.timer-text').innerHTML = brick_font ? textToImage(text) : text;
+}
+
 document.addEventListener('DOMContentLoaded', () => {
-	timer_display = document.querySelector('.timer-numbers');
+	// timer_display = document.querySelector('.timer-text');
 	// reset();
-	timer_display.innerHTML = secsToClock(count);
+	setTimerValue(secsToClock(count));
+	// timer_display.innerHTML = secsToClock(count);
 	ipcRenderer.send('set-timer-text', secsToClock(count));
 });
 
@@ -77,7 +101,8 @@ function timer() {
 		pause();
 		clearInterval(counter);
 		//counter ended, do something here
-		timer_display.innerHTML = secsToClock(0);
+		// timer_display.innerHTML = secsToClock(0);
+		setTimerValue(secsToClock(0));
 		ipcRenderer.send('set-timer-text', secsToClock(0));
 		if (play_end_sound) {
 			end_sound.play();
@@ -94,7 +119,8 @@ function timer() {
 		return;
 	}
 	//update the time on the display
-	timer_display.innerHTML = secsToClock(count);
+	// timer_display.innerHTML = secsToClock(count);
+	setTimerValue(secsToClock(count));
 	ipcRenderer.send('set-timer-text', secsToClock(count));
 
 	//play 30sec warning, if applicable
@@ -130,7 +156,8 @@ function reset() {
 	pause();
 	console.log('Resetting timer');
 	count = RESET_VALUE;
-	timer_display.innerHTML = secsToClock(count);
+	// timer_display.innerHTML = secsToClock(count);
+	setTimerValue(secsToClock(count));
 	ipcRenderer.send('set-timer-text', secsToClock(count));
 }
 
@@ -145,9 +172,8 @@ function toggle() {
 function set_chroma_key(chroma_key) {
     chroma_key_mode = chroma_key;
 	if (chroma_key) {
-		// document.querySelector("#timer-display").className = "display-type timer-display-chroma";
 		document.querySelector('#timer-display').style.backgroundColor = chroma_key_color;
-		document.querySelector('#timer-numbers-wrapper').className = 'timer-numbers-wrapper-chroma';
+		document.querySelector('#timer-text-wrapper').className = `timer-text-wrapper-chroma ${brick_font ? "brick" : ""}`;
 		if (chroma_key_team_info){
 			//show the timer team info strip
 			document.querySelector(".timer-team-info-wrapper").style.display = "";
@@ -157,8 +183,7 @@ function set_chroma_key(chroma_key) {
 			document.querySelector(".timer-team-info-wrapper").style.display = "none";
 		}
 	} else {
-		// document.querySelector("#timer-display").className = "display-type timer-display";
-		document.querySelector('#timer-numbers-wrapper').className = 'timer-numbers-wrapper';
+		document.querySelector('#timer-text-wrapper').className = `timer-text-wrapper ${brick_font ? "brick" : ""}`;
 		document.querySelector('#timer-display').style.backgroundColor = '';
 		//hide the timer team info strip
 		document.querySelector(".timer-team-info-wrapper").style.display = "none";
@@ -230,7 +255,16 @@ ipcRenderer.on('set-end-sound', function(event, arg) {
 });
 
 ipcRenderer.on('set-timer-font', function(event, arg) {
-	document.querySelector('.timer-numbers').style.fontFamily = arg;
+	if (arg == "bricks"){
+		brick_font = true;
+		setTimerValue(secsToClock(count));
+		document.querySelector("#timer-text-wrapper").classList.add("brick");
+	}
+	else{
+		brick_font = false;
+		document.querySelector('.timer-text').style.fontFamily = arg;
+		document.querySelector("#timer-text-wrapper").classList.remove("brick");
+	}
 });
 
 ipcRenderer.on('set-auto-advance', function(event, arg) {
