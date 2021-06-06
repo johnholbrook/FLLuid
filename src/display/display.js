840aef7a2d6418ac1a805c5158cddb7c04260609
John Holbrook <john@johnholbrook.us>
2021-06-06 13:11:52 -0400

    Simplify how the main display works

--- src/display/display.js
+++ src/display/display.js
@@ -1,39 +1,3 @@
-document.addEventListener("DOMContentLoaded", () => {
-    hideAll();
-    document.querySelector("#logos-frame").style.display = "";
-});
-
-function hideAll(){
-    document.querySelectorAll(".display-frame").forEach(frame => {
-        frame.style.display = "none";
-    });
-}
-
-function selectDisplay(disp_name){
-    hideAll();
-    if (disp_name == "none" || disp_name == "logos"){
-        document.querySelector("#logos-frame").style.display = "";
-    }
-    else if (disp_name == "timer"){
-        document.querySelector("#timer-frame").style.display = "";
-    }
-    else if (disp_name == "scores"){
-        document.querySelector("#scores-frame").style.display = "";
-    }
-    else if (disp_name == "other-events"){
-        document.querySelector("#other-events-frame").style.display = "";
-    }
-    else if (disp_name == "schedule"){
-        document.querySelector("#schedule-frame").style.display = "";
-    }
-    else if (disp_name == "intro"){
-        document.querySelector("#intro-frame").style.display = "";
-    }
-    else if (disp_name == "message"){
-        document.querySelector("#message-frame").style.display = "";
-    }
-}
-
 const socket = io();
 socket.on('connect', () => {
     console.log("Connected!")
@@ -43,3 +7,16 @@ socket.on("select-display", name => {
     selectDisplay(name);
     console.log(name);
 });
+
+document.addEventListener("DOMContentLoaded", () => {
+    selectDisplay("logos");
+});
+
+function selectDisplay(disp_name){
+    if (disp_name == "none"){
+        selectDisplay("logos");
+    }
+    else{
+        document.querySelector("#display-frame").src = `/${disp_name}`;
+    }
+}
