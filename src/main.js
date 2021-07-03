771eee3e702e4bdea9a1162bf63d9a95bb56a9a9
John Holbrook <john@johnholbrook.us>
2021-07-03 13:34:45 -0400

    Cleanup in main.js
    Remove references to extra timer window

--- src/main.js
+++ src/main.js
@@ -170,6 +170,8 @@ function createControllerWindow(){
   }
 
   // and load the index.html of the app.
+  controllerWindow.loadFile('./src/controller/controller.html');
+  controllerWindow.setMinimumSize(550, 400);
 }
 
 function createDisplayWindow(){
@@ -200,31 +202,6 @@ ipcMain.on("launch-disp-window", () => {
     });
   }
 });
-
-function createExtraTimerWindow(){
-  extraTimerWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
-    webPreferences: {
-      // preload: path.join(__dirname, 'preload.js')
-      nodeIntegration:true
-    }
-  })
-
-  if (isMac){
-    extraTimerWindow.setTouchBar(touchBar)
-  }
-
-  // and load the index.html of the app.
-  extraTimerWindow.loadFile('./extraTimer/extraTimer.html');
-
-  extraTimerWindow.on('closed', function () {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    extraTimerWindow = null;
-  });
-}
 
 function checkForUpdates(manually_activated){
   var getJSON = require('./web_scraper/get_json.js');
@@ -317,26 +294,15 @@ app.on('activate', function () {
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
 
-//create the extra timer window when prompted by the controller window
-ipcMain.on("spawn-extra-timer-window", function(event, arg){
-  if (extraTimerWindow === null) createExtraTimerWindow();
-});
-
 //distribute the set-timer-text message to appropriate windows
 ipcMain.on("set-timer-text", function(event, arg){
   // controllerWindow.webContents.send("set-timer-text", arg);
-  // if (extraTimerWindow != null){
-  //   extraTimerWindow.webContents.send("set-timer-text", arg);
-  // }
   timer_current.label = arg;
 });
 
 //distribute the set-timer-font message to appropriate windows
 ipcMain.on("set-timer-font", function(event, arg){
   displayWindow.webContents.send("set-timer-font", arg);
-  if (extraTimerWindow != null){
-    extraTimerWindow.webContents.send("set-timer-font", arg);
-  }
 });
 
 ipcMain.on("set-start-button-text", function(event, arg){
