10fa163aaaf449ed63fa002f56ae3d38b56ef41c
John Holbrook <john@johnholbrook.us>
2021-07-23 09:37:30 -0400

    Migrate from electron-builder to electron-packager

---
+++ build.js
@@ -0,0 +1,21 @@
+const packager = require('electron-packager')
+
+async function bundleElectronApp(options) {
+  const appPaths = await packager(options)
+  console.log(`Electron app bundles created:\n${appPaths.join("\n")}`)
+}
+
+let options = {
+    dir: ".",
+    out: "./dist",
+    overwrite: true,
+    platform: ["darwin", "win32"],
+    arch: ["x64", "arm64"],
+    appBundleId: "com.fll-timer.flluid",
+    appCopyright: `Copyright © 2019-${new Date().getFullYear()} John Holbrook`,
+    icon: "./build/app-icon.png",
+    asar: true
+}
+
+
+bundleElectronApp(options);
